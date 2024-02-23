c817a3abf07e545c182f10c3cbf163561ea10216
Miguel Silva <miguelsilvavianaa@gmail.com>
2024-02-22 21:19:39 -0300

    finish project

--- app/stripe/error/page.jsx
+++ app/stripe/error/page.jsx
@@ -1,5 +1,17 @@
+import Link from 'next/link';
 const ErrorPage = () => {
-  return <div>page</div>;
+  return (
+    <section className='py-72'>
+      <div className='container mx-auto'>
+        <h3 className='mb-4 text-center'>
+          Something went wrong! Please try again!
+        </h3>
+        <Link href={'/'}>
+          <button className='mx-auto btn btn-accent'>Back to homepage</button>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
--- app/stripe/success/page.jsx
+++ app/stripe/success/page.jsx
@@ -1,5 +1,17 @@
+import Link from 'next/link';
 const SuccessPage = () => {
-  return <div>page</div>;
+  return (
+    <section className='py-72'>
+      <div className='container mx-auto'>
+        <h3 className='mb-4 text-center'>
+          Your payment was successful! Thank you!
+        </h3>
+        <Link href={'/'}>
+          <button className='mx-auto btn btn-primary'>Back to homepage</button>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default SuccessPage;
