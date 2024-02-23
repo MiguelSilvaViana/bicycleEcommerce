4e0dbc26067cf49a26906832b8ba6eca8919e03a
Miguel Silva <miguelsilvavianaa@gmail.com>
2024-02-22 21:14:02 -0300

    create hero and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,61 @@
+import Link from 'next/link';
+import { Input } from './ui/input';
+import {
+  RiYoutubeFill,
+  RiFacebookFill,
+  RiInstagramFill,
+  RiTwitterFill,
+  RiPinterestFill,
+} from 'react-icons/ri';
 const Footer = () => {
-  return <footer>Footer</footer>;
+  const socialsData = [
+    { icon: <RiYoutubeFill />, link: 'www.youtube.com' },
+    { icon: <RiFacebookFill />, link: 'www.facebook.com' },
+    { icon: <RiInstagramFill />, link: 'www.instagram.com' },
+    { icon: <RiTwitterFill />, link: 'www.twitter.com' },
+    { icon: <RiPinterestFill />, link: 'www.pinterest.com' },
+  ];
+  return (
+    <footer className='pt-12 text-center text-white xl:pt-24 bg-primary'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-8'>
+          <div>
+            <h2 className='mb-2 leading-tight capitalize'>
+              Subscribe to our newsletter
+            </h2>
+            <p className='text-white/60'>
+              Be the first to get the latest news about trends, promotions and
+              much more!
+            </p>
+          </div>
+          {/* form */}
+          <form className='flex flex-col xl:flex-row w-full max-w-[720px] mx-auto gap-5'>
+            <Input placeholder='Your email address' />
+            <button className='btn w-full xl:max-w-[150px] h-[60px] btn-accent'>
+              Join
+            </button>
+          </form>
+          {/* socials */}
+          <div className='flex gap-8 mx-auto text-[20px] text-white/60 mb-20'>
+            {socialsData.map((item, index) => {
+              return (
+                <Link
+                  href={item.link}
+                  key={index}
+                >
+                  {item.icon}
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      {/* Copyright */}
+      <div className='py-6 border-t border-white/5 text-white/60'>
+        Copyright &copy; 2024 Bikezz. All rights reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,51 @@
+import Image from 'next/image';
+import Link from 'next/link';
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className='py-48 md:py-0 md:h-[820px] relative overflow-hidden bg-primary/5'>
+      <div className='container mx-auto'>
+        <div className='flex items-center justify-between'>
+          {/* text */}
+          <div className='w-full xl:max-w-[580px] md:h-[820px] flex flex-col justify-center items-start'>
+            <h1 className='mb-6 text-center xl:text-left'>
+              Where <span>Joyful</span> Cycling Begins
+            </h1>
+            <p className='mb-10 text-lg max-w-[508px] mx-auto text-center xl:text-left xl:mx-0'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Veritatis, laboriosam harum? Dignissimos nulla, omnis ut debitis
+              alias iure quidem nesciunt sapiente ex inventore neque dolorem?
+              Corporis vel quaerat soluta ab.
+            </p>
+            {/*  btn group */}
+            <div className='flex items-center gap-4 mx-auto xl:mx-0'>
+              <Link
+                href={'/our-bikes'}
+                className='mx-auto md:mx-0'
+              >
+                <button className='btn btn-primary'>Shop now</button>
+              </Link>
+              <Link
+                href={'/our-bikes'}
+                className='mx-auto md:mx-0'
+              >
+                <button className='btn btn-accent'>Our bikes</button>
+              </Link>
+            </div>
+          </div>
+          {/* img */}
+          <div className='hidden xl:flex'>
+            <Image
+              src={'/bike.png'}
+              width={765}
+              height={480}
+              alt=''
+              quality={100}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
